5d2db86d48b405b85a49c789ab94483a5d44b5b3
Anay Gandhi <anaygandhi@gmail.com>
2025-03-20 10:15:28 -0500

    added txt search functionality

--- search.py
+++ search.py
@@ -8,8 +8,23 @@ def search_in_pdf(file_path):
     return "".join([page.get_text() for page in doc])
 
 
-# Extracting text from a .docx file
+# Extracting text from a docx file
 def search_in_docx(file_path):
     doc = Document(file_path)
     return "\n".join([para.text for para in doc.paragraphs])
 
+# Extracting text from a txt file
+def search_in_txt(file_path):
+    with open(file_path, "r", encoding="utf-8") as file:
+        return file.read()
+
+# Main searhc in any file type 
+def search_in_file(file_path):
+    if file_path.endswith('.pdf'):
+        return search_in_pdf(file_path)
+    elif file_path.endswith('.docx'):
+        return search_in_docx(file_path)
+    elif file_path.endswith('.txt'):
+        return search_in_txt(file_path)
+    else:
+        raise ValueError("Unsupported file format")
---
+++ search_tests/test_txt_search.py
@@ -0,0 +1,29 @@
+import os
+import sys
+from docx import Document
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from search import search_in_txt
+
+# Creating a test TXT file
+def create_test_txt(filename="test.txt"):
+    with open(filename, "w", encoding="utf-8") as file:
+        file.write("Hello, this is a test TXT file.")
+
+# Testing TXT extraction functionality
+def test_txt_functionality():
+    test_txt = "test.txt"
+    create_test_txt(test_txt)
+    expected_text = "Hello, this is a test TXT file."
+    
+    if os.path.exists(test_txt):
+        extracted_text = search_in_txt(test_txt).strip()
+        print("\nExtracted Text from TXT:\n", extracted_text)
+
+        if expected_text != extracted_text:
+            print(f"Test Failed: Expected '{expected_text}', but got '{extracted_text}'")
+        else:
+            print("Test Passed: Extracted text matches expected content")
+
+# Run  
+if __name__ == "__main__":
+    test_txt_functionality()
