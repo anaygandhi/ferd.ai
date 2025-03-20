e51fe6aa2009606b368c688333206f035c9e3ddb
Anay Gandhi <anaygandhi@gmail.com>
2025-03-20 10:41:29 -0500

    metadata extraction functionality

--- text_extraction.py
+++ text_extraction.py
@@ -29,17 +29,17 @@ def pdf_ocr_extraction(file_path):
     return "\n".join(extracted_text)
 
 
-# Extracting text from a docx file
+# Extracting text from a DOCX file
 def search_in_docx(file_path):
     doc = Document(file_path)
     return "\n".join([para.text for para in doc.paragraphs])
 
-# Extracting text from a txt file
+# Extracting text from a TXT file
 def search_in_txt(file_path):
     with open(file_path, "r", encoding="utf-8") as file:
         return file.read()
 
-# Main searhc in any file type 
+# Main search in any file type 
 def search_in_file(file_path):
     if file_path.endswith('.pdf'):
         return search_in_pdf(file_path)
