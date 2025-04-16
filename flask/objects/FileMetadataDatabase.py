8fe93a080f627f68200f2c0358d35ffd8619c712
jjhealey <jjheals@gmail.com>
2025-04-15 20:23:22 -0400

    add ignored paths functionality

--- flask/objects/FileMetadataDatabase.py
+++ flask/objects/FileMetadataDatabase.py
@@ -4,6 +4,8 @@ import sqlite3 as sql
 import numpy as np 
 import pandas as pd 
 
+from utils import normalize_path
+
 
 class FileMetadataDatabase: 
     
@@ -80,6 +82,9 @@ class FileMetadataDatabase:
     def new_file_entry(self, filepath:str, filename:str, metadata:dict, file_hash:str, embedding_array:np.ndarray) -> None: 
         """Creates a new row in the "file_metadata" table with the given information."""
         
+        # Normalize the filepath 
+        filepath = normalize_path(filepath)
+        
         # Execute INSERT query
         self.cursor.execute(
             '''
@@ -112,3 +117,130 @@ class FileMetadataDatabase:
             self.cursor.fetchall(),
             columns=self.get_table_columns(table_name)
         )
+        
+    
+    def new_ignored_path(self, path:str, type:str) -> None: 
+        """Creates a new entry in the "ignored_paths" table for the given path."""
+        
+        # Convert the given type to lowercase 
+        type = type.lower()
+        
+        # Check that a valid type is given    
+        if not type in ['file', 'directory']: raise AttributeError(f'Given type "{type}" is not valid. Must be one of "file" or "directory".')
+        
+        # Normalize the path format 
+        path = normalize_path(path) 
+        
+        try: 
+            # Insert the new row
+            self.cursor.execute(
+                'INSERT INTO ignore_paths(path, type) VALUES(?, ?)',
+                (path, type)
+            )
+            
+            # Commit changes 
+            self.cxn.commit() 
+        
+        # Handle exceptions
+        except sql.IntegrityError: 
+            # Integrity error means the path is already ignored - do nothing
+            return 
+        
+        
+    def check_path_ignored(self, path:str) -> bool: 
+        """Checks if the given path is ignored. NOTE: the path must actually exist in the filesystem and should be an absolute path."""
+        
+        print('Got path: ', path)
+        
+        # Normalize the path 
+        path = normalize_path(path) 
+        
+        print('Normalized path: ', path) 
+        
+        # Make sure the path exists, return False if not
+        if not os.path.exists(path): return False
+        
+        print('PATH EXISTS') 
+        
+        # Check if the path is a file or directory to optimize the query
+        if os.path.isdir(path): 
+            t:str = 'directory'
+        else: 
+            t:str = 'file'
+        
+        print('Got path type: ', t)
+        
+        # Execute select query
+        self.cursor.execute(
+            "SELECT 1 FROM ignore_paths WHERE type = ? AND ? GLOB path LIMIT 1", 
+            (t, path)
+        )
+        
+        # Fetch results
+        result:int = self.cursor.fetchone()
+        
+        print('Got result: ', result)
+        
+        # Check if we got a direct match
+        if result and result[0] > 0: return True    
+        
+        # If no direct match and it is a file, check if it is in an ignored directory
+        elif t == 'file': 
+            
+            # Get the parent dir
+            parent:str = os.path.dirname(path)
+            
+            # Traverse backwards down the path and keep checking if the parent dir is ignored
+            while parent and parent != '/':
+                
+                # Normalize the path 
+                parent = normalize_path(parent)
+                
+                # Execute query
+                self.cursor.execute(
+                    "SELECT 1 FROM ignore_paths WHERE type = 'directory' AND ? GLOB path LIMIT 1",
+                    (parent,)
+                )
+                
+                # Fetch result
+                result:tuple = self.cursor.fetchone()
+                
+                # Handle result
+                if result:
+                    print(f'Parent directory {parent} is ignored')
+                    return True
+                
+                # If no match, get the parent dir of this dir
+                new_parent:str = os.path.dirname(parent)
+                
+                # Prevent infinite loop at root
+                if new_parent == parent: break
+                
+                # Set the parent as the new parent  
+                parent = new_parent
+
+        # False if we make it here
+        print('No ignore match found')
+        return False
+    
+        
+    def get_ignored_files_in_dir(self, dir_path:str) -> list[str]: 
+        """Returns the filepaths for files in the given dir that are ignored."""
+        
+        # If the dir path does not include a wildcard at the end, then add it
+        if not dir_path.endswith('/*'): 
+            dir_path = dir_path.rstrip('/\\') + '/*'
+        
+        # Execute the query
+        self.cursor.execute(
+            """
+                SELECT path 
+                FROM ignore_paths 
+                WHERE type = ?
+                AND path GLOB ?
+            """,
+            ('file', dir_path)
+        )
+        
+        # Fetch results and return
+        return [r[0] for r in self.cursor.fetchall()]
