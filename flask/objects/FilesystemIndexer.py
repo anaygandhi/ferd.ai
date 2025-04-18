0b090c9cc2df7a342a1505e01239bfdac3a6ce17
jjhealey <jjheals@gmail.com>
2025-04-17 21:43:14 -0400

    add logging to FilesystemIndexer

--- flask/objects/FilesystemIndexer.py
+++ flask/objects/FilesystemIndexer.py
@@ -1,11 +1,11 @@
 import os 
 import faiss 
-import sqlite3 as sql
 import numpy as np 
+import logging 
 
 from sentence_transformers import SentenceTransformer 
 from .FileMetadataDatabase import FileMetadataDatabase
-from utils import print_log, extract_metadata, hash_file_sha256, read_file
+from utils import print_log, extract_metadata, hash_file_sha256, read_file, setup_logger
 
 
 class FilesystemIndexer: 
@@ -15,14 +15,21 @@ class FilesystemIndexer:
     index_bin_path:str                       # Path to the Faiss index binary file 
     embedding_dim:int                        # Embedding dimensions for the index
     sentence_transformer:SentenceTransformer # Sentence transformer model for indexing files
+    logger:logging.Logger                    # Logger for saving robust logs
     
     
-    def __init__(self, start_dir:str, metadata_db_path:str, index_bin_path:str, embedding_dim:int): 
+    def __init__(self, start_dir:str, metadata_db_path:str, index_bin_path:str, embedding_dim:int, log_filepath:str='logs/filesystem_indexer.log'): 
         self.start_dir = start_dir
         self.file_metadata_db = FileMetadataDatabase(metadata_db_path)
         self.index_bin_path = index_bin_path
         self.embedding_dim = embedding_dim
         self.sentence_transformer = SentenceTransformer('all-MiniLM-L6-v2')  
+        
+        # Set up a logger for the FilesystemIndexer
+        self.logger = setup_logger(
+            log_filepath,
+            'filesystem_indexer'
+        )
         
     
     def index_filesystem(self, overwrite:bool=False, verbose:bool=False, save_frequency:int=2) -> None: 
@@ -48,8 +55,9 @@ class FilesystemIndexer:
         # Check if overwriting existing data 
         if overwrite: 
             
-            # Info print 
+            # Info print & log
             if verbose: print_log('WARN', 'index_filesystem()', f'Overwriting existing index at "{self.index_bin_path}".')
+            self.logger.warning(f'in index_filesystem() - overwritting existing index at "{self.index_bin_path}".')
             
             # Delete the existing index 
             if os.path.exists(self.index_bin_path): os.remove(self.index_bin_path)
@@ -60,16 +68,18 @@ class FilesystemIndexer:
         # If NOT overwriting, then just make the SQL connection and read the existing index
         else: 
             
-            # Info print
+            # Info print & log
             if verbose: print_log('INFO', 'index_filesystem()', f'Reading existing index at "{self.index_bin_path}".')
+            self.logger.info(f'in index_filesystem() - reading existing index at "{self.index_bin_path}".')
             
             # Read the existing index or create it if it doesn't exist
             if os.path.exists(self.index_bin_path): index:faiss.IndexFlatL2 = faiss.read_index(self.index_bin_path)
             else: index:faiss.IndexFlatL2 = faiss.IndexFlatL2(self.embedding_dim)
             
         # ---- Indexing ---- #
-        # Info print
+        # Info print & log
         if verbose: print_log('INFO', 'Filesystem.index_filesystem()', f'Starting indexing from "{self.start_dir}". Files: {os.listdir(self.start_dir)}')
+        self.logger.info(f'in index_filesystem() - starting indexing from "{self.start_dir}"')
         
         # Iterate over all the child dirs in [self.start_dir] and recursively index each file
         for root, dirs, files in os.walk(self.start_dir):
@@ -77,12 +87,14 @@ class FilesystemIndexer:
             # Check if this dir is ignored 
             if self.file_metadata_db.check_path_ignored(root): 
                 
-                # Info print and do nothing else 
+                # Info print & log and do nothing else 
                 if verbose: print_log('INFO', 'FilesystemIndexer.index_filesystem()', f'Skipping directory "{root}" because the path is ignored in DB.')
+                self.logger.info(f'in index_filesystem() - skipping directory "{root}" because the path is ignored in DB.')
                 continue 
             
-            # Info print 
+            # Info print & log
             if verbose: print_log('INFO', 'FilesystemIndexer.index_filesystem()', f'Reading {len(files)} from {root}.')
+            self.logger.info(f'in index_filesystem() - reading {len(files)} from {root}.')
             
             # Iterate over all the files in this dir
             for file in files:
@@ -100,15 +112,21 @@ class FilesystemIndexer:
                 # Save after file index if configured (level 3)
                 if save_frequency == 3: 
                     faiss.write_index(index, self.index_bin_path)
+                    
+                    # Info print & log
                     if verbose: print_log('SUCCESS', 'FilesystemIndexer.index_filesystem()', f'FAISS index saved to "{self.index_bin_path}"') 
-            
+                    self.logger.info(f'in index_filesystem() - FAISS index saved to "{self.index_bin_path}" (save_frequency = 3)')
+                    
             # Save after dir index if configured (level 2) 
             faiss.write_index(index, self.index_bin_path)
             if verbose: print_log('SUCCESS', 'FilesystemIndexer.index_filesystem()', f'FAISS index saved to "{self.index_bin_path}"') 
-                
+            self.logger.info(f'in index_filesystem() - FAISS index saved to "{self.index_bin_path}" (save_frequency = 2)')
+            
         # Save after filesystem is indexed if configured (level 1) 
         faiss.write_index(index, self.index_bin_path)
         if verbose: print_log('SUCCESS', 'FilesystemIndexer.index_filesystem()', f'FAISS index saved to "{self.index_bin_path}"') 
+        self.logger.info(f'in index_filesystem() - FAISS index saved to "{self.index_bin_path}" (save_frequency = 1)')
+        
             
     def index_file(self, filepath:str, index:faiss.IndexFlatL2, verbose:bool=False) -> None: 
         """Reads the file at the given path, extracts the metadata and other info, hashes the file, and stores the results in the
@@ -128,8 +146,9 @@ class FilesystemIndexer:
         # Check the file extension and ignore invalid files
         if not filepath.endswith(('.pdf', '.docx', '.txt')): 
             
-            # Info print and do not index the file
+            # Info print & log and do not index the file
             if verbose: print_log('INFO', 'index_file()', f'Ignoring file (invalid extension) "{filepath}".')
+            self.logger.info(f'in index_file() - ignoring file (invalid extension) "{filepath}".')
             return  
 
         try:
@@ -139,6 +158,7 @@ class FilesystemIndexer:
                 
                 # Info print 
                 if verbose: print_log('INFO', 'index_file()', f'Ignoring file (path ignored in DB) "{filepath}".')
+                self.logger.info(f'in index_file() - ignoring file (path ignored in DB) "{filepath}".')
                 
                 # Do nothing else 
                 return  
@@ -155,8 +175,9 @@ class FilesystemIndexer:
                 # Check if the hashes match
                 if file_hash == existing_entry['file_sha256']: 
                     
-                    # Info print and do not index the file
+                    # Info print & log and do not index the file
                     if verbose: print_log('INFO', 'FilesystemIndexer.index_file()', f'ignoring "{filepath}" since it already exists with the same hash.')
+                    self.logger.info(f'ignoring "{filepath}" since it already exists with the same hash.')
                     return  
 
                 # If hashes do not match, delete the existing entry so we can make a new one
@@ -164,6 +185,7 @@ class FilesystemIndexer:
                     
                     # Info print 
                     if verbose: print_log('INFO', 'FilesystemIndexer.index_file()', f'File "{filepath}" already exists in the database but with a different hash - updating DB entry.')
+                    self.logger.info(f'in index_file() - "{filepath}" already exists in the database but with a different hash - updating DB entry.')
                     
                     # Delete existing row
                     self.file_metadata_db.delete_file_entry(filepath)
@@ -176,8 +198,9 @@ class FilesystemIndexer:
             # Check the embedding 
             if embedding is None or len(embedding) != self.embedding_dim:
                 
-                # Info print (warn) and do nothing else 
-                print_log('WARN', 'FilesystemIndexer.index_file()', f'Failed to generate valid embedding for "{filepath}". Skipping.')
+                # Info print & log (warn) and do nothing else 
+                if verbose: print_log('WARN', 'FilesystemIndexer.index_file()', f'Failed to generate valid embedding for "{filepath}". Skipping.')
+                self.logger.warning(f'in index_file() - failed to generate valid embedding for "{filepath}". Skipping.')
                 return 
             
             # Convert the embedding to an array and add to the index
@@ -195,12 +218,16 @@ class FilesystemIndexer:
 
         # Handle exceptions
         except Exception as e:
-            print_log('ERROR', 'FilesystemIndexer.index_file()', f"Error processing {filepath}. Caught exception: {e.__class__} - {e}")
+            if verbose: print_log('ERROR', 'FilesystemIndexer.index_file()', f"Error processing {filepath}. Caught exception: {e.__class__} - {e}")
+            self.logger.error(f'in index_file() - error processing {filepath}. Caught exception: {e.__class__} - {e}')
             
             
     def search_files(self, query:str, top_k:int=5) -> list[int]:
         """Searches the index for the given query and returns the top_k matched file IDs."""
 
+        # Log
+        self.logger.info(f'in search_files() - starting search in index for query: "{query}".')
+        
         # If the index filepath doesn't exist, then we cannot search, so return empty list
         if not os.path.exists(self.index_bin_path): return []
         
@@ -215,17 +242,25 @@ class FilesystemIndexer:
 
         # Check shape of the query embedding 
         if query_embedding.shape[1] != self.embedding_dim:
-            print("\033[91mERROR in search_files(): \033[0mQuery embedding has incorrect dimensions. Aborting search.")
+            print_log('ERROR', 'FilesystemIndexer.search_files()', "Query embedding has incorrect dimensions. Aborting search.")
+            self.logger.error('in search_files() - query embedding has incorrect dimensions. Aborting search.')
             return []
 
         # Search the index for the query embedding for the top_k documents 
         _, indices = index.search(query_embedding, top_k)
-
+        
+        # Log
+        self.logger.info(f'in search_files() - search for "{query}" completed with matched file IDs: {indices}.')
+        
         # Return the matched indices
         return indices
     
 
     def search_subset(self, query:str, file_ids_to_include:list[int], top_k:int=5) -> list[int]:
+        """Searches the index for the query, only considering the given subset of file IDs."""
+        
+        # Log
+        self.logger.info(f'in search_subset() - searching index for "{query}" with {len(file_ids_to_include)} given IDs to include.')
         
         # If the index filepath doesn't exist OR if no file_ids_to_include are given, 
         # then we cannot search, so return empty list
@@ -254,5 +289,11 @@ class FilesystemIndexer:
         # Search the index
         _, temp_indices = temp_index.search(query_embedding, top_k)
 
-        # Map temporary indices back to real file IDs and return
-        return [file_ids_to_include[i] for i in temp_indices[0]]
+        # Map temporary indices back to real file IDs 
+        matched_file_ids:list[int] = [file_ids_to_include[i] for i in temp_indices[0]]
+        
+        # Log
+        self.logger.info(f'in search_subset() - completed search for "{query}" and got results: {matched_file_ids}.')
+        
+        # Return the real file IDs
+        return matched_file_ids
