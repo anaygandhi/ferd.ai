d2d9dcb96b301509d860cfc2326e47ebaa9d594d
jjhealey <jjheals@gmail.com>
2025-04-18 00:01:12 -0400

    add endpoints for adding/deleting ignored paths

--- flask/blueprints/filesystem_bp.py
+++ flask/blueprints/filesystem_bp.py
@@ -73,3 +73,102 @@ def get_ignored_dirs():
     return jsonify({
         'ignored_paths': metadata_db.get_all_ignored_paths()
     })
+    
+
+@fs_bp.route('/add-ignored-path', methods=['POST']) 
+def add_ignored_path(): 
+    """
+    Takes in a path and adds it to the DB as an ignored path.
+    
+    Request body: 
+        { 
+            "path": "/some/path/to/ignore/" 
+        }
+        
+    Returns: 
+        200 | success | Path successfully added to the DB to be ignored. 
+        400 | bad request | Not given a path OR the path does not exist in the filesystem (check error in response JSON for context).
+        409 | conflict | The given path is already ignored, either by inheritance or explicitly. 
+    """
+    
+    # Get the path from the request body
+    request_json:dict = request.get_json()
+    new_path:str = request_json.get('path', None)
+    
+    # Verify a valid path was given
+    # Check if given a path at all
+    if not new_path: 
+        return jsonify({
+            'error': 'Not given a path to ignore'
+        }), 400
+        
+    # Check that the given path exists
+    elif not os.path.exists(new_path): 
+        return jsonify({
+            'error': f'Given path "{new_path}" does not exist.'
+        }), 400
+        
+    # Get the FileMetadataDatabase from the current app
+    file_metadata_db:FileMetadataDatabase = current_app.file_metadata_db
+    
+    # Check that the path isn't already ignored 
+    if file_metadata_db.check_path_ignored(new_path): 
+        return jsonify({
+            'error': 'Path is already ignored (either by inheritance or explicitly).'
+        }), 409
+    
+    # Check if the new path is a file or directory
+    if os.path.isdir(new_path): type:str = 'directory'
+    else: type:str = 'file'
+    
+    # Add the new path to the DB 
+    file_metadata_db.new_ignored_path(new_path, type)
+    
+    # Return success
+    return jsonify({
+        'message': f'Path "{new_path}" is now being ignored by indexing.'
+    }), 200
+    
+    
+@fs_bp.route('/del-ignored-path', methods=['POST']) 
+def del_ignored_path(): 
+    """
+    Deletes the given path from the "ignored_paths" table in the DB so it will be indexed in the future.
+    
+    Request body: 
+        { 
+            "path": "/some/path/"
+        }
+        
+    Returns: 
+        200 | success | Path was successfully removed from the ignored paths table and will be indexed in the future.
+        400 | bad request | Not given a path OR the given path does not exist in the filesystem. 
+    """
+    
+    # Get the path from the request body
+    request_json:dict = request.get_json()
+    path:str = request_json.get('path', None)
+    
+    # Verify a valid path was given
+    # Check if given a path at all
+    if not path: 
+        return jsonify({
+            'error': 'Not given a path to delete.'
+        }), 400
+        
+    # Check that the given path exists
+    elif not os.path.exists(path): 
+        return jsonify({
+            'error': f'Given path "{path}" does not exist in the filesystem.'
+        }), 400
+        
+    # Get the FileMetadataDatabase from the current app
+    file_metadata_db:FileMetadataDatabase = current_app.file_metadata_db
+    
+    # Delete the entry from the DB ignored_paths table
+    file_metadata_db.del_ignored_path(path)
+    
+    # Return 
+    return jsonify({
+        'message': f'Path "{path}" is no longer ignored.'
+    })
