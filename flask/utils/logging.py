5567059bb141338c1b94fa8b25f9693d96725b85
jjhealey <jjheals@gmail.com>
2025-04-17 21:24:51 -0400

    add func to setup a logger

--- flask/utils/logging.py
+++ flask/utils/logging.py
@@ -1,3 +1,6 @@
+import logging
+import os
+
 from .general import now 
 
 
@@ -26,3 +29,31 @@ def print_log(level:str, loc:str, msg:str) -> None:
     
     # Print the log
     print(f'\033[0m[{now()}] {color}{level} in {loc}: \033[0m{msg}')
+    
+
+def setup_logger(log_file_path:str, logger_name:str, min_level:int=logging.DEBUG, log_format:str='%(asctime)s - %(levelname)s: %(message)s') -> logging.Logger:
+    """Sets up a logger to save logs to the given filepath."""
+    
+    # Init a logger and set the lowest level to DEBUG (so all logs are captured)
+    logger:logging.Logger = logging.getLogger(logger_name)
+    logger.setLevel(min_level)
+    
+    # Prevent double logging if root logger is used
+    logger.propagate = False  
+
+    # Avoid duplicate handlers if setup is called multiple times
+    if not logger.handlers:
+        
+        # Create the output dir if it doesn't exist
+        os.makedirs(os.path.dirname(log_file_path), exist_ok=True)
+
+        # Create a file handler
+        file_handler:logging.FileHandler = logging.FileHandler(log_file_path, encoding='utf-8')
+        logger.addHandler(file_handler)
+        
+        # Set the format for logs 
+        formatter:logging.Formatter = logging.Formatter(log_format)
+        file_handler.setFormatter(formatter)
+        
+    # Return the logger
+    return logger
